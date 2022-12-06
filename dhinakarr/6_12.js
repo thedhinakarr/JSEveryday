429cc7458f9e9ff90a16d631ff351349733749e1
Dinakar <dvk@Dinakars-MacBook-Pro.local>
2022-12-06 10:56:40 +0530

    That's it for the day

--- dhinakarr/6_12.js
+++ dhinakarr/6_12.js
@@ -1,11 +1,44 @@
-//Array
+//Array size without methods.
 
 let arr =[1,2,3,4,5,6];
-console.log(arr[3]);
+var count =0;
+var i=0;
+
+while (arr[i]!= undefined) {
+    count++;
+    i++;
+}
+console.log(count);
+
+let arr2 = new Array(10);
+
+for(var i=0; i<10;i++){
+    arr2[i]=0;
+}
+console.log(arr2);
+
+//Accessing last element
+console.log(arr[arr.length-1]);
+console.log(arr.at(-1));
+
+//shift
+arr.shift();
+//unshift
+arr.unshift();
+
+//pop
+arr.pop();
+//push
+arr.push(10);
+
+console.log(arr);
 
 
+//OBJECTS
 
-var mixed = [1,2,3,"A","B","V","D"];
-console.log(mixed);
-console.log(mixed[0]+mixed[3]);
-console.log(mixed[0]===mixed[3]);
+var x ={
+    a:1,
+    b:2,
+    c:["HI","BYE","GOODBYE"],
+}
+console.log(x.c[2]);
