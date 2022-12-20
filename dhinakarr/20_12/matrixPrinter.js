7f607186e7e415a4301ec7280f7a657e5125fa2e
Dinakar <dvk@Dinakars-MacBook-Pro.local>
2022-12-20 11:24:17 +0530

    20_12

--- dhinakarr/20_12/matrixPrinter.js
+++ dhinakarr/20_12/matrixPrinter.js
@@ -1,13 +1,25 @@
+import chalk from 'chalk';
+
 function printMatrix(matrix){
 
     for(let i=0;i<matrix.length;i++){
         let s=''
         for(let j=0;j<matrix.length;j++){
-            s+=matrix[i][j];s+=' '
+        
+           if(i==j || (i+j===matrix.length-1)){
+            s+=chalk.greenBright(matrix[i][j]);s+=' ';
+           }
+           else{
+               s+=matrix[i][j];s+=' '
+            }
+            
         }
+     
         console.log(s);
     }
 
 }
 
 export default printMatrix;
+
+//Object destructuring --> Research.
