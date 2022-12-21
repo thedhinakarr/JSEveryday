532b58fa3d1fc215a4b5580bdd35b70daf9261f7
Dinakar <dvk@Dinakars-MacBook-Pro.local>
2022-12-21 15:42:06 +0530

    21_12

---
+++ dhinakarr/21_12/cw.js
@@ -0,0 +1,18 @@
+let str = "hello";
+
+let reverse = "";
+
+for(let i=str.length-1; i>=0;i--){
+    reverse+= str[i];
+}
+
+console.log(reverse)
+
+
+var arr = new Array();
+
+for(let i=0;i<50;i++){
+    arr.push(i+1);
+}
+
+console.log(arr);
